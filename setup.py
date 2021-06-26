f23f21eafc7e267d8fa1abd1fd34cf5134db1867
Eric Wimberley <wimberleyeric@gmail.com>
2021-06-25 22:14:37 -0400

    Added additional release information to the setup.py file.

--- setup.py
+++ setup.py
@@ -2,9 +2,19 @@
 
 from distutils.core import setup
 
-setup(name='yarp-parser',
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(name='yarp_parser',
       version='0.1',
       description='YARP - Yet Another Recursive Parser.',
       author='Eric Wimberley',
       packages=['yarp_parser'],
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/ewimberley/yarp",
+      project_urls={
+          "Bug Tracker": "https://github.com/ewimberley/yarp/issues",
+      },
+      python_requires=">=3.6",
       )
